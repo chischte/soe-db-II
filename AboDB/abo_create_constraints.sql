7fe9dbc838930b68b1d5a02750c19ffa463acb57
michi <chischte@gmx.ch>
2020-10-08 14:46:29 +0200

    Add a default constraint

--- AboDB/abo_create_constraints.sql
+++ AboDB/abo_create_constraints.sql
@@ -11,3 +11,14 @@ alter table Ort
 
 alter table AboArt
 	add constraint UQ_Aboart_Bezeichnung unique(bezeichnung)
+
+-- Check constraints
+
+alter table Ort
+	add constraint CK_Ort_PLZ check (PLZ between 1000 and 9999);
+
+-- Default constraint
+
+alter table AboArt
+	add constraint DF_AboArt_Gebuehr
+	default 0.0 for Gebuehr;
