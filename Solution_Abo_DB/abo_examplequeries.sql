ceb6ccc501359baf31f045e963968c2feca5a458
michi <chischte@gmx.ch>
2020-10-18 19:59:53 +0200

    Add example queries

--- Solution_Abo_DB/abo_examplequeries.sql
+++ Solution_Abo_DB/abo_examplequeries.sql
@@ -1,7 +1,20 @@
 use abo
 
-SELECT	Mitglied.MitgliedID, Mitglied.AnredeID, AboArt.Bezeichnung
+SELECT	AnredeText, Mitglied.MitgliedID, Mitglied.AnredeID, AboArt.Bezeichnung
 FROM    AboArt INNER JOIN
 		Mitglied ON AboArt.AboArtID = Mitglied.AboArtID INNER JOIN
-        Anrede ON Mitglied.AnredeID = Anrede.AnredeID INNER JOIN
-        Ort ON Mitglied.OrtID = Ort.OrtID
+		Anrede ON Mitglied.AnredeID = Anrede.AnredeID INNER JOIN
+		Ort ON Mitglied.OrtID = Ort.OrtID
+
+
+SELECT  Mitglied.MitgliedID, Mitglied.Nachname, Mitglied.Vorname, Anrede.AnredeText
+FROM    AboArt INNER JOIN
+		Mitglied ON AboArt.AboArtID = Mitglied.AboArtID INNER JOIN
+		Anrede ON Mitglied.AnredeID = Anrede.AnredeID INNER JOIN
+		Ort ON Mitglied.OrtID = Ort.OrtID
+		WHERE  (Anrede.AnredeText = 'Herr')
+
+
+
+
+
