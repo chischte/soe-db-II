f548a0cb975294e686076905afd961b14d2c81f7
michi <chischte@gmx.ch>
2020-10-21 19:51:36 +0200

    Create tables in AboDB

--- AboDB/mw_abo_create_scheme.sql
+++ AboDB/mw_abo_create_scheme.sql
@@ -1,22 +1,59 @@
 
 if(db_id('abo') is null)
 begin
-create database abo;
+create database Abo;
 end
 go
 
-use abo;
+use Abo;
 go
 
-drop table aboart;
+
+drop table Mitglied;
 go
 
-create table aboart
+create table Mitglied
 (
-    aboartID        int identity(1,1)   not null,
-    abobezeichnung  varchar(20)         not null,
-    abogebuehr      decimal(6,2)        
+    MitgliedID      int identity(1,1)   not null,
+    AnredeID        int,
+    OrtID           int                 not null,
+    AboID           int,
+    Vorname         varchar(20)         not null,
+    Nachname        varchar(20)         not null,
+    Eintritt        date                not null
+
+    constraint PK_Mitglied primary key(MitgliedID)
+);
+go
+
+drop table Aboart;
+go
+
+create table Aboart
+(
+    AboartID        int identity(1,1)   not null,
+    Abobezeichnung  varchar(20)         not null,
+    Abogebuehr      decimal(6,2)        
     
-    constraint pk_aboart primary key(aboartID)
+    constraint PK_Aboart primary key(AboartID)
+);
+go
+
+create table Ort
+(
+    OrtID       int  identity(1,1)	not null,
+    OrtName     varchar(20)			not null,
+    PLZ			int,
+	
+	constraint PK_Ort primary key(OrtID)
+);
+go
+			
+create table Anrede
+(
+	AnredeID	int identity(1,1)	not null,
+	AnredeText	varchar(20)			not null
+
+	constraint PK_Anrede primary key(AnredeID)
 );
 go
