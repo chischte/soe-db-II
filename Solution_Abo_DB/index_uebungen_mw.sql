c529c39a8128f7a80b0198d2532751cfb1a2f3e1
michi <chischte@gmx.ch>
2020-10-22 16:15:24 +0200

    Add some indices

---
+++ Solution_Abo_DB/index_uebungen_mw.sql
@@ -0,0 +1,26 @@
+use abo
+
+create index IX_mitglied_ort
+	on Mitglied(OrtID);
+
+create index idx_mitglied_aboID
+	on Mitglied(AboArtID);
+
+alter index all on Mitglied rebuild;
+
+create index idx_ortort
+	on ort(Ortschaft);
+
+alter index all on ort rebuild;
+
+--Beispiel vorname und nachname suchen
+
+create index idx_MitgliedVornameNachname
+	on Mitglied(Nachname, Vorname);
+
+
+select nachname, vorname
+	from mitglied	
+	where nachname = 'Pozzi';
+
+
